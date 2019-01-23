3fb08128e7721f3ad2a5deacd4ead1fafeac8b8d
Tyler Maschino <tylermaschino@gmail.com>
2019-01-23 05:47:30 -0500

    Added create, read, delete for Page, Link, and Product types

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -10,6 +10,114 @@ const Mutations = {
     );
     return user;
   },
+
+  async deleteUser(parent, args, context, info) {
+    const user = await context.db.mutation.deleteUser(
+      {
+        where: {
+          ...args,
+        },
+      },
+      info
+    );
+    return user;
+  },
+
+  async createDomain(parent, args, context, info) {
+    const domain = await context.db.mutation.createDomain(
+      {
+        data: {
+          ...args,
+        },
+      },
+      info
+    );
+    return domain;
+  },
+
+  async deleteDomain(parent, args, context, info) {
+    const domain = await context.db.mutation.deleteDomain(
+      {
+        where: {
+          ...args,
+        },
+      },
+      info
+    );
+    return domain;
+  },
+
+  async createPage(parent, args, context, info) {
+    const page = await context.db.mutation.createPage(
+      {
+        data: {
+          ...args,
+        },
+      },
+      info
+    );
+    return page;
+  },
+
+  async deletePage(parent, args, context, info) {
+    const page = await context.db.mutation.deletePage(
+      {
+        where: {
+          ...args,
+        },
+      },
+      info
+    );
+    return page;
+  },
+
+  async createLink(parent, args, context, info) {
+    const link = await context.db.mutation.createLink(
+      {
+        data: {
+          ...args,
+        },
+      },
+      info
+    );
+    return link;
+  },
+
+  async deleteLink(parent, args, context, info) {
+    const link = await context.db.mutation.deleteLink(
+      {
+        where: {
+          ...args,
+        },
+      },
+      info
+    );
+    return link;
+  },
+
+  async createProduct(parent, args, context, info) {
+    const product = await context.db.mutation.createProduct(
+      {
+        data: {
+          ...args,
+        },
+      },
+      info
+    );
+    return product;
+  },
+
+  async deleteProduct(parent, args, context, info) {
+    const product = await context.db.mutation.deleteProduct(
+      {
+        where: {
+          ...args,
+        },
+      },
+      info
+    );
+    return product;
+  },
 };
 
 module.exports = Mutations;
--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -3,6 +3,18 @@ const { forwardTo } = require('prisma-binding');
 const Query = {
   users: forwardTo('db'),
   user: forwardTo('db'),
+
+  domains: forwardTo('db'),
+  domain: forwardTo('db'),
+
+  pages: forwardTo('db'),
+  page: forwardTo('db'),
+
+  links: forwardTo('db'),
+  link: forwardTo('db'),
+
+  products: forwardTo('db'),
+  product: forwardTo('db'),
 };
 
 module.exports = Query;
