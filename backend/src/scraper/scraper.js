6fefe27e3d27b70892f9b60f63e11fd24c98cd74
Matthew Meinzer <meinzer.matthew@gmail.com>
2019-01-22 22:37:04 -0600

    Finish parseMarkup and parseHref functions

--- backend/src/scraper/scraper.js
+++ backend/src/scraper/scraper.js
@@ -2,10 +2,25 @@ const cheerio = require("cheerio");
 
 function parseMarkup(markup) {
   const $ = cheerio.load(markup);
-  const links = $('a[href!=""]:not([href^=#],[href^="/"])')
-    .toArray()
-    .map(link => link.attribs);
-  return { links };
+  const links = $('a[href!=""]:not([href^=#])')
+    .map(function() {
+      const node = $(this);
+      return {
+        href: node.attr("href"),
+        text: node.text().trim(),
+        rel: node.attr("rel") || null,
+        title: node.attr("title") || null
+      };
+    })
+    .toArray();
+  return links;
 }
 
-module.exports.parseMarkup = parseMarkup;
+function parseHref(href) {
+  const url = new URL(href);
+  const { hostname, pathname, protocol, hash } = url;
+  const params = new Map(url.searchParams);
+  return { hostname, pathname, protocol, hash, params };
+}
+
+module.exports = { parseMarkup, parseHref };
--- backend/src/scraper/tests/scraper.test.js
+++ backend/src/scraper/tests/scraper.test.js
@@ -1,16 +1,30 @@
-const { parseMarkup } = require("../scraper");
+const { parseMarkup, parseHref } = require("../scraper");
 const { markup } = require("./mockSite");
 
 describe("parseMarkup function", () => {
-  test("exists and is imported", () => {
-    expect(parseMarkup).toBeDefined();
-  });
   const result = parseMarkup(markup);
-  console.log(result.links);
   test("returns an object", () => {
     expect(typeof result).toBe("object");
   });
   test("parses 54 links", () => {
-    expect(result.links.length).toBe(45);
+    expect(result.length).toBe(45);
+  });
+});
+
+describe("parseHref function", () => {
+  const href =
+    "https://www.amazon.com/Fellow-EKG-Pour-over-Temperature-Stopwatch/dp/B077JBQZPX?psc=1&SubscriptionId=AKIAIPF6TXJESDTZH4HQ&tag=triplebar-20&linkCode=xm2&camp=2025&creative=165953&creativeASIN=B077JBQZPX";
+  const result = parseHref(href);
+  test("returns an object", () => {
+    expect(typeof result).toBe("object");
+  });
+  test("returns with the correct hostname", () => {
+    expect(result.hostname).toBeDefined();
+    expect(result.hostname).toBe("www.amazon.com");
+  });
+  test("returns a parameters Map that has a `tag` key of the correct value", () => {
+    expect(result.params).toBeDefined();
+    expect(result.params.has("tag")).toBe(true);
+    expect(result.params.get("tag")).toBe("triplebar-20");
   });
 });
