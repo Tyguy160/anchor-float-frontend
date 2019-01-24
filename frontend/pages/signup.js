93a22736206056e51f3e121811524b78bfb660e6
Tyler Maschino <tylermaschino@gmail.com>
2019-01-24 08:06:23 -0500

    Added signup page

---
+++ frontend/pages/signup.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Signup from '../components/Signup';
+
+const signup = props => {
+  return (
+    <div>
+      <Signup />
+    </div>
+  );
+};
+
+export default signup;
