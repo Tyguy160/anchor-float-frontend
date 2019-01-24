35e3f7207e2f764a22f596af76ddfe25b8fc3425
Matt <meinzer.matthew@gmail.com>
2019-01-24 07:28:06 -0600

    Remove unwanted field on db query

--- backend/src/index.js
+++ backend/src/index.js
@@ -29,7 +29,7 @@ server.express.use(async (req, res, next) => {
   if (!req.userId) return next();
   const user = await db.query.user(
     { where: { id: req.userId } },
-    '{ id, permissions, email, name }'
+    '{ id, email, name }'
   );
   req.user = user;
   next();
