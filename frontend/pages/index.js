5a4690b3b41f7b3f7b60a77b9161c3bbc62d90e2
Tyler Maschino <tylermaschino@gmail.com>
2019-01-26 09:11:10 -0500

    Created new hero display with CTA

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,9 +1,68 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const HeroContainer = styled.div`
+  color: white;
+  background-image: url('../static/blake-connally-373084-unsplash.jpg');
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+  height: 100vh;
+  display: grid;
+  justify-items: center;
+`;
+
+const HeroHeadline = styled.div`
+  display: grid;
+  font-size: 3em;
+  text-align: center;
+  align-self: end;
+  margin-bottom: 100px;
+  text-shadow: 1px 1px 3px #888;
+`;
+
+const CTA = styled.div`
+  > button {
+    margin: 20px;
+    padding: 10px 20px 10px 20px;
+    box-shadow: 1px 1px 1px 1px #c4941c;
+    cursor: pointer;
+    font-size: 1.5em;
+    font-family: 'Quicksand', sans-serif;
+    background-color: goldenrod;
+    border-radius: 2px;
+    border: none;
+  }
+`;
 
 const Home = props => {
   return (
     <div>
-      <p>This is the home page</p>
+      <HeroContainer>
+        <HeroHeadline>
+          <div>You've worked hard to make valuable content</div>
+          <div>Protect Your Asset</div>
+        </HeroHeadline>
+        <CTA>
+          <button>Learn more</button>
+          <button>Sign up</button>
+        </CTA>
+      </HeroContainer>
+      <div>
+        <h2>What is Affiliate Shield?</h2>
+      </div>
+
+      <div>
+        <h2>What You Get</h2>
+      </div>
+
+      <div>
+        <h2>How It Works</h2>
+      </div>
+
+      <div id="pricing">
+        <h2>Pricing</h2>
+      </div>
     </div>
   );
 };
