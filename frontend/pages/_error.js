c0c305101bf9e750c231e80f19663bfd2232b288
Tyler Maschino <tylermaschino@gmail.com>
2019-01-26 15:19:22 -0500

    Added custom error page

---
+++ frontend/pages/_error.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const ErrorMesage = styled.div`
+  display: grid;
+  height: 100%;
+  align-items: center;
+  justify-items: center;
+  padding: 20px;
+`;
+
+export default class Error extends React.Component {
+  static getInitialProps({ res, err }) {
+    const statusCode = res ? res.statusCode : err ? err.statusCode : null;
+    return { statusCode };
+  }
+
+  render() {
+    return (
+      <ErrorMesage>
+        <h2>{this.props.statusCode}</h2>
+        <p>Uh oh...something went wrong. That page doesn't seem to exist 🤷‍</p>
+      </ErrorMesage>
+    );
+  }
+}
