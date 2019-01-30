937a0f93a83e46c78695a053a9265e830def97f6
Matt <meinzer.matthew@gmail.com>
2019-01-29 23:55:21 -0600

    Domains query only shows user added domains

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -14,7 +14,21 @@ const Query = {
     );
   },
 
-  domains: forwardTo('db'),
+  async domains(parent, args, context, info) {
+    const { userId } = context.request;
+    if (!userId) {
+      throw new Error('You must be logged in');
+    }
+    const user = await context.db.query.user(
+      {
+        where: {
+          id: userId,
+        },
+      },
+      `{ domains { id, hostname } }`
+    );
+    return user.domains;
+  },
   domain: forwardTo('db'),
 
   pages: forwardTo('db'),
