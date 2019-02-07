c60db584e54d15c896f2cd3634df89ea34c7e3d5
Matthew Meinzer <meinzer.matthew@gmail.com>
2019-02-07 00:19:18 -0600

    Remove unused function

--- backend/src/scraper/utils.js
+++ backend/src/scraper/utils.js
@@ -2,9 +2,4 @@ function constructProductUrl({ asin }) {
   return `https://www.amazon.com/dp/${asin}`;
 }
 
-function isAmazonProductLink(href) {
-  console.log('WARNING: isAmazonProductLink always returns true');
-  return true;
-}
-
-module.exports = { constructProductUrl, isAmazonProductLink };
+module.exports = { constructProductUrl };
