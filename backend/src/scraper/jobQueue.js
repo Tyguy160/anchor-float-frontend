f0dc0431a03708a0d91b7552f6e3cc21a0fe6815
Matthew Meinzer <meinzer.matthew@gmail.com>
2019-02-07 00:19:58 -0600

    Rate limit Amazon scraping

--- backend/src/scraper/jobQueue.js
+++ backend/src/scraper/jobQueue.js
@@ -1,8 +1,15 @@
 const Queue = require('bull');
 const path = require('path');
 
+const TIME = 15000; // 15 seconds
+
 const pageParseQueue = new Queue('page-parsing');
-const productParseQueue = new Queue('product-parsing');
+const productParseQueue = new Queue('product-parsing', {
+  limiter: {
+    max: 1,
+    duration: TIME,
+  },
+});
 
 pageParseQueue.process(path.join(__dirname, './pageParseProcessor.js'));
 productParseQueue.process(path.join(__dirname, './productParseProcessor.js'));
