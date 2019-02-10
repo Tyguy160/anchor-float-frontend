ac1a1f67b89d587d3356be4637094833c1fee01d
Matt <meinzer.matthew@gmail.com>
2019-02-10 16:59:05 -0600

    Make site page processing async

--- backend/src/scraper/sitemapParseProcessor.js
+++ backend/src/scraper/sitemapParseProcessor.js
@@ -13,42 +13,50 @@ async function sitemapParseProcessor(job) {
     const sitemap = new Sitemapper();
     const { sites } = await sitemap.fetch(sitemapUrl);
 
-    sites.forEach(async pageUrl => {
-      const { hostname, pathname } = new URL(pageUrl);
-      console.log(
-        `Making new page for... | ${new Date().toUTCString()}\n${pathname}\n`
-      );
-      const page = await db.mutation.createPage(
-        {
-          data: {
-            url: pageUrl,
-          },
-        },
-        `{ id }`
-      );
-
-      await db.mutation.updateDomain(
-        {
-          where: { hostname },
-          data: {
-            pages: { connect: [{ id: page.id }] },
-          },
-        },
-        `{ id }`
-      );
-
-      pageParseQueue.add({
-        url: pageUrl,
-        pageId: page.id,
-        contentSelector,
-      });
-    });
+    for (pageHref of sites) {
+      await createPageAndConnect(pageHref);
+    }
 
     return Promise.resolve(sites);
   } catch (error) {
     console.log(error);
     return Promise.reject(error);
   }
+}
+
+async function createPageAndConnect(pageUrl) {
+  const { hostname, pathname } = new URL(pageUrl);
+  console.log(
+    `Making new page for... | ${new Date().toUTCString()}\n${pathname}\n`
+  );
+  const page = await db.mutation.createPage(
+    {
+      data: {
+        url: pageUrl,
+      },
+    },
+    `{ id }`
+  );
+  console.log(`Created page | ${new Date().toUTCString()}\n${page.id}\n`);
+
+  const theDomain = await db.mutation.updateDomain(
+    {
+      where: { hostname },
+      data: {
+        pages: { connect: [{ id: page.id }] },
+      },
+    },
+    `{ id }`
+  );
+  console.log(
+    `Updated domain | ${new Date().toUTCString()}\n${theDomain.id}\n`
+  );
+
+  // pageParseQueue.add({
+  //   url: pageUrl,
+  //   pageId: page.id,
+  //   contentSelector,
+  // });
 }
 
 module.exports = sitemapParseProcessor;
