e5c25633286374396917f2a34adca56389d6fcaa
Matt Meinzer <meinzer.matthew@gmail.com>
2019-08-09 00:19:32 -0500

    Add page markup parser with tests and file struct

---
+++ backend/scrape/workers/page.js
@@ -0,0 +1,5 @@
+async function scrapePage(url) {
+  return true;
+}
+
+module.exports = { scrapePage };
---
+++ backend/scrape/workers/sitemap.js
@@ -0,0 +1,5 @@
+async function scrapeSitemap(url) {
+  return true;
+}
+
+module.exports = { scrapeSitemap };
