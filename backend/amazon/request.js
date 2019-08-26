0c3940a85aaa4418a627ffff4efd7c93c579a714
Matt Meinzer <meinzer.matthew@gmail.com>
2019-08-26 18:05:52 -0500

    Add time as ISO function

--- backend/amazon/request.js
+++ backend/amazon/request.js
@@ -1,5 +1,11 @@
+const url = require('url');
+
 const BASE_URL = 'http://webservices.amazon.com/onca/xml';
 
 function getFullRequestURL(asin) {
   const stringToSign = getStringToSign(asin);
+}
+
+function timeAsIso() {
+  return `${new Date().toISOString().split('.')[0]}Z`;
 }
