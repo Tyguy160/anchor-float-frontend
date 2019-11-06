b402ffec3135d672a9ae67453be9889c2adef509
Matt Meinzer <meinzer.matthew@gmail.com>
2019-11-06 10:46:29 -0600

    Tweak bucket upload function

--- backend/report/workers/utils.js
+++ backend/report/workers/utils.js
@@ -1,3 +1,5 @@
+const { Storage } = require('@google-cloud/storage');
+
 function getDataFromMessage(messageBody, dataKey) {
   const body = JSON.parse(messageBody);
   if (!(dataKey in body)) {
@@ -8,35 +10,18 @@ function getDataFromMessage(messageBody, dataKey) {
 }
 
 async function uploadFile(bucketName, filename) {
-  // [START storage_upload_file]
-  // Imports the Google Cloud client library
-  const { Storage } = require('@google-cloud/storage');
-
-  // Creates a client
   const storage = new Storage();
 
-  /**
-   * TODO(developer): Uncomment the following lines before running the sample.
-   */
-  // const bucketName = 'Name of a bucket, e.g. my-bucket';
-  // const filename = 'Local file to upload, e.g. ./local/path/to/file.txt';
-
-  // Uploads a local file to the bucket
   await storage.bucket(bucketName).upload(filename, {
-    // Support for HTTP requests made with `Accept-Encoding: gzip`
     gzip: true,
-    // By setting the option `destination`, you can change the name of the
-    // object you are uploading to a bucket.
+    destination: 'some-random-bucket-name',
     metadata: {
-      // Enable long-lived HTTP caching headers
-      // Use only if the contents of the file will never change
-      // (If the contents will change, use cacheControl: 'no-cache')
       cacheControl: 'public, max-age=31536000',
+      contentDisposition: 'attachment; filename="amazon-link-report.csv"',
     },
   });
 
   console.log(`${filename} uploaded to ${bucketName}.`);
-  // [END storage_upload_file]
 }
 
 module.exports = { getDataFromMessage, uploadFile };
