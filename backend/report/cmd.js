3369a80eb5c56d3dd4593be80de9bfef43215077
Matt Meinzer <meinzer.matthew@gmail.com>
2019-11-07 02:04:30 -0600

    Add more env var checks

--- backend/report/cmd.js
+++ backend/report/cmd.js
@@ -3,9 +3,7 @@ const uuid = require('uuid/v4');
 const { reportProducer } = require('./producers.js');
 
 const hostnameInput = process.argv.slice(2, 3)[0];
-const userId = 'ck2mks10y00001fs1e9kbv1fc';
-
-// const userInputUrl = `https://${sitemapUrl}`;
+const userId = 'test';
 
 console.log(`Generating a report for: ${hostnameInput}`);
 
