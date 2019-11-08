74f4b29942d52865d6549a41dee92a98e9552f51
Tyler Maschino <tylermaschino@gmail.com>
2019-11-08 05:32:21 -0500

    Added error/success messages to display status to user

--- backend/api/resolvers/Mutation.js
+++ backend/api/resolvers/Mutation.js
@@ -3,6 +3,10 @@ const { randomBytes } = require('crypto');
 const { promisify } = require('util');
 const stripe = require('stripe')('sk_test_b0MPemUYvtnsaU6aaHzyMKUA');
 
+// Sitemap parsing imports
+const uuid = require('uuid/v4');
+const { sitemapProducer } = require('../../scan/producers');
+
 const { transport, emailTemplate } = require('../mail');
 const { getUserTokenFromId } = require('../user');
 const {
@@ -407,10 +411,33 @@ const Mutation = {
         throw new Error('There was an issue finding your account details');
       });
     const accountCredits = dbUser.creditsRemaining;
+    console.log(accountCredits);
 
     if (accountCredits > 0) {
       // Run the report
       console.log('Running 🏃');
+      sitemapProducer.send(
+        [
+          {
+            id: uuid(),
+            body: JSON.stringify({ url: 'https://www.triplebarcoffee.com/sitemap.xml' }),
+          },
+        ],
+        (err) => {
+          if (err) console.log(err);
+        },
+      );
+      await db.users.update({
+        where: {
+          id: user.userId,
+        },
+        data: {
+          creditsRemaining: accountCredits - 1,
+        },
+      });
+      console.log(
+        `You've queued up a report and now you only have ${accountCredits - 1} credits left`,
+      );
     } else {
       throw new Error("You don't have enough credits to generate this report");
     }
