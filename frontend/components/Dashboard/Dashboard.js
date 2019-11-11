ba08a11604f781edce68b5de12bc4231f899d6f8
Tyler Maschino <tylermaschino@gmail.com>
2019-11-10 19:10:05 -0500

    Removed unnecessary imports and comments

--- frontend/components/Dashboard/Dashboard.js
+++ frontend/components/Dashboard/Dashboard.js
@@ -1,56 +1,15 @@
 import React, { useState } from 'react';
-import styled from 'styled-components';
-import { useQuery, useLazyQuery } from '@apollo/react-hooks';
-import {
-  USERSITES_QUERY,
-  SITEPAGES_QUERY,
-  GET_CURRENT_USER,
-} from '../resolvers/resolvers';
-import { PageSection } from '../styles/styles';
+import { useQuery } from '@apollo/react-hooks';
+import { USERSITES_QUERY, GET_CURRENT_USER } from '../resolvers/resolvers';
 import AddDomain from './AddDomain';
 import DomainSettings from './DomainSettings';
 import DomainData from './DomainData';
 import DomainSelection from './DomainSelection';
-
-const DashboardContainer = styled.div`
-  display: flex;
-  flex-flow: row wrap;
-  margin: 20px;
-`;
-
-const DomainSettingsContainer = styled.div`
-  flex: 1;
-  order: 2;
-`;
-
-const DomainDataContainer = styled.div`
-  flex: 2;
-  order: 1;
-  min-width: 60%;
-`;
-
-const StyledTable = styled.table`
-  padding: 20px;
-  width: 100%;
-  table-layout: auto;
-  text-align: right;
-  border-collapse: collapse;
-  th,
-  td {
-    padding-top: 8px;
-    padding-bottom: 8px;
-  }
-  th:nth-child(1) {
-    text-align: left;
-  }
-  td:nth-child(1) {
-    text-align: left;
-    padding-left: 20px;
-  }
-  tr {
-    border-bottom: 1px darkgray solid;
-  }
-`;
+import {
+  DashboardContainer,
+  DomainSettingsContainer,
+  DomainDataContainer,
+} from '../styles/styles';
 
 const Dashboard = () => {
   const { loading: domainsLoading, data: userSites } = useQuery(
@@ -64,9 +23,6 @@ const Dashboard = () => {
 
   // These hooks are for the selected user site, not the domain name
   const [selectedUserSite, setSelectedUserSite] = useState();
-
-  const [domain, setDomain] = useState('');
-  const [newDomain, setNewDomain] = useState(null);
 
   return (
     <DashboardContainer>
