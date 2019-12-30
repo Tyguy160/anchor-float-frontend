6be46cbdf16f4cf825751ec47e497ed37d110273
Matt Meinzer <meinzer.matthew@gmail.com>
2019-12-30 15:26:16 -0500

    Add prod endpoint url

--- config.js
+++ config.js
@@ -2,3 +2,4 @@
 const host = process.env.DOCKERIZED ? 'api' : 'localhost'; // is the app being ran in docker?
 
 export const endpoint = `http://${host}:4000/graphql`;
+export const productionEndpoint = `http://api.anchorfloat.com/graphql`;
