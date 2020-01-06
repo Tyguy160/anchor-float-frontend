2862cca66841a3f7c79a49faa54b77a39582602b
Tyler Maschino <tylermaschino@gmail.com>
2020-01-05 20:33:28 -0500

    Changed production endpoint to https

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
-const host = process.env.DOCKERIZED ? 'api' : 'localhost'; // is the app being ran in docker?
+const host = process.env.DOCKERIZED ? "api" : "localhost"; // is the app being ran in docker?
 
 export const endpoint = `http://${host}:4000/graphql`;
-export const productionEndpoint = `http://api.anchorfloat.com/graphql`;
+export const productionEndpoint = `https://api.anchorfloat.com/graphql`;
